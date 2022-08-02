1db691034267371f09c57fe2de7891e678078f0b
chelcx2 <100275141+chelcx2@users.noreply.github.com>
2022-08-02 16:57:24 -0700

    file
    dockerfile

--- network.py
+++ network.py
@@ -4,8 +4,6 @@ import socket
 import tqdm
 import os
 
-SEPARATOR = "<SEPARATOR>"
-BUFFER_SIZE = 4096 # send 4096 bytes each time step
 
 """ here we specify the IP address, port of the server we will try to connect to, and the name of the files we will be sending  """
 # the ip address or hostname of the server, the receiver
