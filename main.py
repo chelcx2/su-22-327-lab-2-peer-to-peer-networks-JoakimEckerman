a9e2f3648b2d54dd6bad9085606512add66562b8
chelcx2 <100275141+chelcx2@users.noreply.github.com>
2022-08-03 14:32:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 
-import zmq, time, pickle, sys, socket
-
-
+import socket
 
 localHost = "127.0.0.1" 
 port = 1234  # Port to listen on (non-privileged ports are > 1023)
@@ -28,22 +26,3 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket1:
                     break
                 conn.sendall(data)
 
-
-context = zmq.Context()
-me = str(sys.argv[1])
-s = context.socket(zmq.PUSH) # create a push socket
-src = SRC1 if me == '1' else SRC2 # check task source host
-prt = PORT1 if me == '1' else PORT2 # check task source port
-p1 = "tcp://"+ SRC1 +":"+ PORT1 # address first task source
-p2 = "tcp://"+ SRC2 +":"+ PORT2 # address second task source
-s.bind(p1) # bind socket to address 1
-s.bind(p2) # bind socket to address 2
-s.connect(p1) # connect to task source 1
-s.connect(p2) # connect to task source 2
-
-while True: # do stuff
-    pass
-    #workload = random.randint(1, 100) # compute workload
-    #s.send(pickle.dumps((me,workload))) # send workload to worker
-    #work = pickle.loads(r.recv()) # receive work from a source
-    #time.sleep(work[1]*0.01) # pretend to work
