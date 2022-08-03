9c467e3b68bc22807498a4d1e95d78a2dd362afb
Joakim <joakim.eckerman01@student.csulb.edu>
2022-08-02 18:39:06 -0700

    update, non functional

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ me = str(sys.argv[1])
 s = context.socket(zmq.PUSH) # create a push socket
 src = SRC1 if me == '1' else SRC2 # check task source host
 prt = PORT1 if me == '1' else PORT2 # check task source port
-p = "tcp://"+ src +":"+ prt # how and where to connect
 p1 = "tcp://"+ SRC1 +":"+ PORT1 # address first task source
 p2 = "tcp://"+ SRC2 +":"+ PORT2 # address second task source
-s.bind(p) # bind socket to address
+s.bind(p1) # bind socket to address 1
+s.bind(p2) # bind socket to address 2
 s.connect(p1) # connect to task source 1
 s.connect(p2) # connect to task source 2
 
-while True: # generate 100 workloads
+while True: # do stuff
     pass
     #workload = random.randint(1, 100) # compute workload
     #s.send(pickle.dumps((me,workload))) # send workload to worker
