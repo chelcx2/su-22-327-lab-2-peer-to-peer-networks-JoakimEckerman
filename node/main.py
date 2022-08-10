3562e950657e2826bda34b584d9759fc2f621b40
chelcx2 <100275141+chelcx2@users.noreply.github.com>
2022-08-09 18:41:46 -0700

    Alternative Client/ Server method
    Trying a dif client/server method and renamed files

--- node/main.py
+++ node/main.py
@@ -1,12 +1,76 @@
 
 import sys, os
+import socket
+from threading import Thread
 from socket import *
-from ping3 import ping
+
 
 
 print ("starting...")
 
-s = socket(AF_INET, SOCK_STREAM)
+def server():
+    serversock = socket.socket()
+    host = socket.gethostname();
+    port = 9000;
+    serversock.bind((host,port));
+    filename = ""
+    serversock.listen(10);
+    print ("Waiting for a connection.....")
+
+    clientsocket,addr = serversock.accept()
+    print("Got a connection from %s" % str(addr))
+    while True:
+        size = clientsocket.recv(16) # Note that you limit your filename length to 255 bytes.
+        if not size:
+            break
+        size = int(size, 2)
+        filename = clientsocket.recv(size)
+        filesize = clientsocket.recv(32)
+        filesize = int(filesize, 2)
+        file_to_write = open(filename, 'wb')
+        chunksize = 4096
+        while filesize > 0:
+            if filesize < chunksize:
+                chunksize = filesize
+            data = clientsocket.recv(chunksize)
+            file_to_write.write(data)
+            filesize -= len(data)
+
+        file_to_write.close()
+        print ('File received successfully')
+    serversock.close()
+
+def client():
+        
+    s = socket.socket()
+    host = socket.gethostname()
+    port = 9000
+    s.connect((host, port))
+    path = "blah"
+    directory = os.listdir(path)
+    for files in directory:
+        print (files)
+        filename = files
+        size = len(filename)
+        size = bin(size)[2:].zfill(16) # encode filename size as 16 bit binary
+        s.send(size)
+        s.send(filename)
+
+        filename = os.path.join(path,filename)
+        filesize = os.path.getsize(filename)
+        filesize = bin(filesize)[2:].zfill(32) # encode filesize as 32 bit binary
+        s.send(filesize)
+
+        file_to_send = open(filename, 'rb')
+
+        l = file_to_send.read()
+        s.sendall(l)
+        file_to_send.close()
+        print ('File Sent')
+
+    s.close()
+
+'''   s = socket(AF_INET, SOCK_STREAM)
 s.bind(("", 1234))
 s.listen()
 try:
@@ -21,7 +85,12 @@ except gaierror:
 listIP = []
 for num in range(2,6):
     ip = "172.30.0."+str(num)
-
+'''Each device on the network can safely run a service on the same port. EX >>
+172.17.0.1:1234
+172.17.0.2:1234
+172.17.0.3:1234
+172.17.0.4:1234
+'''
     if ping(ip):
         if not ip in listIP:
             listIP.append(ip)
@@ -54,6 +123,7 @@ except gaierror:
     sys.exit()
 except error:
     print("\ Server not responding !!!!")
-    sys.exit()
+    sys.exit()    '''
+
 
 print ("exiting...")
