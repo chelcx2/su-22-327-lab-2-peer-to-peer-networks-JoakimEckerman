011098ea7c6ffebb90901b174a4f7d7308601231
Joakim <joakim.eckerman01@student.csulb.edu>
2022-08-10 20:21:37 -0700

    All files being requested, but file transfer doesn't trigger

--- node/main.py
+++ node/main.py
@@ -3,7 +3,6 @@ import sys, os, traceback, time
 from threading import Thread
 from socket import *
 from tracemalloc import stop
-from ping3 import ping
 
 
 print ("starting...")
@@ -19,11 +18,12 @@ BUFFER_SIZE = 4096
 def broadcastRequest():
     s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
     s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-    s.sendto(b"", ("255.255.255.255", 1234))
+    s.sendto(b"List all files.", ("255.255.255.255", 1234))
     s.close()
 
-def broadcastListen():
+def broadcastListen(stop):
     s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
+    s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
     s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     s.bind(("", 1234))
     while True:
@@ -33,49 +33,61 @@ def broadcastListen():
         if newIP not in listIP and newIP != gethostbyname(gethostname()):
             listIP.append(newIP)
 
-        #if stop():
-        #    break
+        if stop():
+            break
 
         msg = msg.decode("utf-8")
-    s.close()
-'''
         if msg == "List all files.":
             print("Sending list of files")
             for file in files:
                 s.sendto(file.encode("utf-8"), (newIP, 1234))
-        else:
-            if msg not in files:
-                requestFiles.append(msg)
-'''
+        elif msg in files and newIP != gethostbyname(gethostname()):
+            print("I have the file!")
+            t1 = Thread(target = sendFile, args = (msg, newIP, ))
+            t1.start()
+            t1.join()  
+        elif msg not in files:
+            requestFiles.append(msg)
+            print("Requesting " + msg)
+            t1 = Thread(target = requestFile, args = (msg, ))
 
-def sendFile():
-    for node in listIP:
-        s = socket(AF_INET, SOCK_STREAM)
-        print("Connecting to " + node)
-        s.connect((node, 1234))
-        for file in files:
-            print("Sending " + file)
-            filesize = os.path.getsize("/files/" + file)
-            s.send(f"{file}{SEPARATOR}{filesize}".encode("utf-8"))
-            with open("/files/" + file, "rb") as f:
-                while True:
-                    # read the bytes from the file
-                    bytes_read = f.read(BUFFER_SIZE)
-                    if not bytes_read:
-                        # file transmitting is done
-                        break
-                    # we use sendall to assure transimission in 
-                    # busy networks
-                    s.sendall(bytes_read)
-                    #time.sleep(1)
+                    
     s.close()
 
-def recieveFile():
+def sendFile(filename, node):
     s = socket(AF_INET, SOCK_STREAM)
+    s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+    s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    print("Connecting to " + node)
+    s.connect((node, 1234))
+    print("Sending " + filename)
+    filesize = os.path.getsize("/files/" + filename)
+    s.send(f"{filename}{SEPARATOR}{filesize}".encode("utf-8"))
+    with open("/files/" + filename, "rb") as f:
+        while True:
+            # read the bytes from the file
+            bytes_read = f.read(BUFFER_SIZE)
+            if not bytes_read:
+                # file transmitting is done
+                break
+            # we use sendall to assure transimission in 
+            # busy networks
+            s.sendall(bytes_read)
+            #time.sleep(1)
+    s.close()
+
+def requestFile(filename):
+    s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
+    s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
     s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     s.bind(("", 1234))
     s.listen()
+
+    s.sendto(filename.encode("utf-8"), ("255.255.255.255", 1234))
     clientSocket, clientIP = s.accept()
+    print("Connected to " + clientIP)
+    
+    
     received = clientSocket.recv(BUFFER_SIZE).decode()
     filename, filesize = received.split(SEPARATOR)
     filesize = int(filesize)
@@ -90,24 +102,25 @@ def recieveFile():
                     break
                 # write to the file the bytes we just received
                 f.write(bytes_read)
-        #requestFiles.remove(filename)
+        requestFiles.remove(filename)
     time.sleep(1)
     s.close()
 
         
 
-#stop = False
-
-time.sleep(1)
-Thread(target = broadcastListen).start()
+stop = False
+time.sleep(5)
+Thread(target = broadcastListen, args = (lambda: stop,)).start()
 time.sleep(1)
 Thread(target = broadcastRequest).start()
-#time.sleep(1)
-#Thread(target = broadcastListen, args = (lambda: stop,)).start()
-#stop = True
 time.sleep(1)
-Thread(target = recieveFile).start()
-Thread(target = sendFile).start()
+Thread(target = broadcastListen, args = (lambda: stop,)).start()
+print(listIP)
+
+stop = True
+
+#Thread(target = recieveFile).start()
+#Thread(target = sendFile).start()
 
 
 
